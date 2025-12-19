3070998548905561ffb43146fce92548670a14ef
Wix Remote Machine <support@wix.com>
2025-12-19 23:20:37 +0000

    Implement UserDirectory Backend

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -334,28 +334,3 @@ export interface UserVerification {
   /** @wixFieldType text */
   verifiedByAdmin?: string;
 }
-
-
-/**
- * Collection ID: userdirectory
- * Interface for UserDirectory
- */
-export interface UserDirectory {
-  _id: string;
-  _createdDate?: Date;
-  _updatedDate?: Date;
-  /** @wixFieldType text */
-  memberId?: string;
-  /** @wixFieldType text */
-  fullName?: string;
-  /** @wixFieldType text */
-  email?: string;
-  /** @wixFieldType datetime */
-  createdDate?: Date | string;
-  /** @wixFieldType datetime */
-  lastSeen?: Date | string;
-  /** @wixFieldType text */
-  verificationStatus?: string;
-  /** @wixFieldType boolean */
-  isActive?: boolean;
-}
