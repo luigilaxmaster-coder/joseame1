25d75a248cd4aa9efdead25e8642e954498eb786
Wix Remote Machine <support@wix.com>
2026-02-04 11:41:59 +0000

    Fix build error

--- src/astro.config.mjs
+++ src/astro.config.mjs
@@ -2,12 +2,16 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import { getViteConfig } from '@wix/vite-plugin';
 
+const viteConfig = getViteConfig();
+
 export default defineConfig({
   integrations: [react()],
   vite: {
-    ...getViteConfig(),
+    ...viteConfig,
     build: {
+      ...(viteConfig.build || {}),
       rollupOptions: {
+        ...(viteConfig.build?.rollupOptions || {}),
         external: ['wix-data', 'wix-members-backend'],
       },
     },
